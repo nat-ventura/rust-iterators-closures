a74a320c8b1ae040e0b1bc2a404ce5cedf228a24
Natalie Ventura Villasana <nat@storj.io>
2018-02-26 19:03:44 -0500

    extracts calls to 'simulated_expensive_calc' to one place and stores result in 'expensive_result' variable

--- expensive_calc/src/main.rs
+++ expensive_calc/src/main.rs
@@ -3,33 +3,37 @@ use std::time::Duration;
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
-    thread::sleep(Duration::from_sec(2));
+    thread::sleep(Duration::from_secs(2));
     intensity
 }
 
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
+}
 
-    generate_workout(intensity: u32, random_number: u32) {
-        if intensity < 25 {
-            println!(
-                "Today, do {} pushups!",
-                simulated_expensive_calculation(intensity)
-            );
-            println!(
-                "Next, do {} situps!",
-                simulated_expensive_calculation(intensity)
-            );
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result =
+        simulated_expensive_calculation(intensity);
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
         } else {
-            if random_number == 3 {
-                println!("Take a break today! Remember to stay hydrated!");
-            } else {
-                println!(
-                    "Today, run for {} minutes!",
-                    simulated_expensive_calculation(intensity)
-                );
-            }
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            );
         }
     }
 }
+
