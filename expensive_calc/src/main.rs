8c2deac5bcdc78a9672c0eb37cea47187b14c1ef
Natalie Ventura Villasana <nat@storj.io>
2018-03-02 14:15:19 -0500

    adds Cacher struct and closure inst

--- expensive_calc/src/main.rs
+++ expensive_calc/src/main.rs
@@ -1,10 +1,33 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
 }
 
 fn main() {
@@ -13,17 +36,20 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result =
-        simulated_expensive_calculation(intensity);
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_result
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -31,9 +57,8 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result
+                expensive_result.value(intensity)
             );
         }
     }
 }
-
